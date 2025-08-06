600e86920f86c673002edefe59be86463721480e
SurajShettigar <suri.shettigar@gmail.com>
2025-08-06 15:26:42 +0530

    Add vcpkg for third-party dependecies. Remove git submodules.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,16 +1,3 @@
-# CPPCodec (Base64 encoding-decoding)
-# Link: https://github.com/tplgy/cppcodec
-add_library(cppcodec INTERFACE)
-target_include_directories(cppcodec INTERFACE ext/cppcodec)
-
-# spdlog (Logging)
-# Link: https://github.com/gabime/spdlog
-add_subdirectory(ext/spdlog)
-
-# Glaze (JSON)
-#Link: https://stephenberry.github.io/glaze/
-add_subdirectory(ext/glaze)
-
 # Vulkan - Graphics API
 # Link: https://www.lunarg.com/vulkan-sdk/
 find_package(Vulkan REQUIRED)
@@ -30,44 +17,27 @@ if (${Vulkan_FOUND})
     endif ()
 endif ()
 
-# Python
-# Link: https://www.python.org/
-find_package(Python REQUIRED)
+# CPPCodec (Base64 encoding-decoding)
+# Link: https://github.com/tplgy/cppcodec
+find_path(CPPCODEC_INCLUDE_DIRS "cppcodec")
+if (CPPCODEC_INCLUDE_DIRS STREQUAL "")
+    message(FATAL_ERROR "Library not found: cppcodec")
+endif ()
+add_library(cppcodec INTERFACE)
+target_include_directories(cppcodec INTERFACE ${CPPCODEC_INCLUDE_DIRS})
+
+# Glaze (JSON)
+# Link: https://stephenberry.github.io/glaze/
+find_package(glaze CONFIG REQUIRED)
+
+# spdlog (Logging)
+# Link: https://github.com/gabime/spdlog
+find_package(spdlog CONFIG REQUIRED)
+
+## OpenImageIO
+## Link: https://github.com/AcademySoftwareFoundation/OpenImageIO
+find_package(OpenImageIO CONFIG REQUIRED)
 
 # OpenUSD
 # Link: https://github.com/PixarAnimationStudios/OpenUSD
-set(OpenUSD_ROOT_DIR ${PROJECT_SOURCE_DIR}/ext/OpenUSD)
-set(OpenUSD_BUILD_SCRIPT_FILE ${OpenUSD_ROOT_DIR}/build_scripts/build_usd.py)
-set(OpenUSD_BUILD_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
-set(OpenUSD_INCLUDE_DIR ${OpenUSD_BUILD_OUTPUT_DIR}/include)
-set(OpenUSD_LIB_DIR ${OpenUSD_BUILD_OUTPUT_DIR}/lib)
-set(OpenUSD_BIN_DIR ${OpenUSD_BUILD_OUTPUT_DIR}/bin)
-set(OpenUSD_CONFIG_FILE ${OpenUSD_BUILD_OUTPUT_DIR}/pxrConfig.cmake)
-
-if (NOT EXISTS ${OpenUSD_CONFIG_FILE})
-# TODO: Build USDView by creating a python virtual environment and executing OpenUSD build script.
-    message("Building OpenUSD...")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        execute_process(
-                COMMAND ${Python_EXECUTABLE}
-                ${OpenUSD_BUILD_SCRIPT_FILE}
-                ${OpenUSD_BUILD_OUTPUT_DIR}
-                --build-variant=debug
-                --no-python
-                --no-examples
-                --no-tutorials
-                --no-tools)
-    else ()
-        execute_process(
-                COMMAND ${Python_EXECUTABLE}
-                ${OpenUSD_BUILD_SCRIPT_FILE}
-                ${OpenUSD_BUILD_OUTPUT_DIR}
-                --no-python
-                --no-examples
-                --no-tutorials
-                --no-tools)
-    endif ()
-else ()
-    message("Skipping OpenUSD build")
-endif ()
-include(${OpenUSD_CONFIG_FILE})
+find_package(pxr CONFIG REQUIRED)
